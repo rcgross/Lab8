75c2818080a9c5694620ac76935aa54cc3b9b0b3
DejaPierce <54558526+DejaPierce@users.noreply.github.com>
2022-03-27 17:22:47 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,4 +1,18 @@
 
 public class Library {
+  private Book book;
+  private Account account;
+  
+  public Library (Book book, Account account) {
+    this.book = book;
+    this.account = account;
+  }
+  
+  public void waitlist(Account a, book b) {
+    
 
+}
+  
+  public void checkout(Account a, Book b) {
+  }  
 }

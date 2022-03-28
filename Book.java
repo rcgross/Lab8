e8af329da24930e60aba33f05ca0258497a5e9b1
ProvidSH <53474565+ProvidSH@users.noreply.github.com>
2022-03-28 17:02:49 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,21 +3,16 @@ import java.util.ArrayList;
 public class Book {
     private int BookID; // Book ID number
     private int RackNumber; // Book Section/Rack number
-    private boolean available = true; // this will indicate if a book is available, set to true if the book is available
+    private String author; //Author name
+    private String title; //Title of book
+    private int amount; //# of books available
+    									// this will indicate if a book is available, set to true if the book is available
                                         // for the library class if it is not available then put on waitlist in main class
                                         // if it is available then member can check out the book
- 
-    private ArrayList<Integer> waitList; // creates a list for the waitlist 
+    
+    private ArrayList<Member> waitList; // creates a list of PEOPLE waiting for the book
     
     // All the setter and getters for the instance variables
-    public void setAvailable(boolean available)
-    {
-        this.available = available;
-    }
-
-    public boolean getAvailable() {
-        return available;
-    }
 
     public void setBookID(int BookID) {
         this.BookID = BookID;
@@ -53,13 +48,44 @@ public class Book {
             return title;
     }
     
+    public int getAmount() {
+    	return amount;
+    }
+    
+    public void setAmount(int amount) {
+    	this.amount = amount;
+    }
+    
+    public ArrayList <Member> getWL() { //get waitlist
+    	return waitList;
+    }
+    
+    public void addtoWL(Member m) { //add member to waitlist on a specific book
+    	waitList.add(m);
+    }
+    
+    public String printwaitList() {
+      String result = "";
+  	  for (int i = 0; i < waitList.size(); i++) {
+  		  if (i == waitList.size() - 1) {
+  			  result.concat("and " + waitList.get(i).getName());
+  		  }
+  		  else {
+  			result.concat(waitList.get(i).getName() + ", "); 
+  		  }
+  	  }
+  	  return result;
+    }
+    
  
 
     // constructor for a book, since each book has an ID number as well as a Rack Number
-    public Book (int bookID, int RackNumber, boolean available) {
+    public Book (int bookID, int RackNumber, String author, String title, int amount) {
         this.BookID = BookID;
         this.RackNumber = RackNumber;
-        this.available = available;
+        this.author = author;
+        this.title = title;
+        this.amount = amount;
         waitList = new ArrayList<>();
     }
 

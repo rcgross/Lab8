f7ef09fc31207e5fcb7f4d44d60c94562c42ea0f
ProvidSH <53474565+ProvidSH@users.noreply.github.com>
2022-03-28 17:03:07 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,28 @@
 public class Account {
 
-  
+	// instance variables
+    private String name;
+    private int memberID;
+    
+    public Account (String name, int memberID) {
+    	this.name = name;
+    	this.memberID = memberID;
+    }
+    
+    public String getName() {
+    	return name;
+    }
+
+    public void setName(String n){
+    	this.name = name;
+    }
+
+    public int getMemberID() {
+    	return memberID;
+    }
+
+    public void setMemberID(int id) {
+    	memberID = id;
+    }
   
 }

86aae3062dc93b1baee33a330d7f325b67f289ce
ProvidSH <53474565+ProvidSH@users.noreply.github.com>
2022-03-28 17:01:51 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,23 +1,58 @@
+import java.util.ArrayList;
 
 public class Library {
-  private Book book;
-  private Account account;
+  private ArrayList<Book> listOfBooks; //list of books in Library
+  private ArrayList<Book> waitlist; //waitlist of BOOKS
   
-  public Library (Book book, Account account) {
-    this.book = book;
-    this.account = account;
+  public Library () {
+	listOfBooks = new ArrayList<Book>();
+    waitlist = new ArrayList<Book>();
   }
   
+  public ArrayList<Book> getLOB() { //get list of books
+	  return listOfBooks;
+  }
+  
+  public void setLOB(ArrayList<Book> b) { //set a list of books
+	  listOfBooks = b;
+  }
+  
+  public ArrayList<Book> getWaitlist() { //get waitlist
+	  return this.waitlist;
+  }
+  
+  public void setWaitList(ArrayList<Book> b) { //sets waitlist
+	  waitlist = b;
+  }
+  
+  public void addwaitList(Book b) {
+	  waitlist.add(b);
+  }
+  
+  
+  
   public static void main(String[] args) {
-    Member mem1 = new Member("Harriet", 4);
-    Book book1 = new Book(3, 34, false);
-    Member mem2 = new Member("j", 2);
-
-    mem1.checkout(book1);
-    mem2.checkout(book1);
-
-    System.out.println("The wait list includes members: " + book1.getWaitList());
-    System.out.println("You are in place " + mem2.waitListStatus(book1) + " on the wait list");
+	  
+	Book b1 = new Book (12345, 2, "Amor Towles", "The Sun Also Rises", 2);
+	Book b2 = new Book (54321, 1, "John Sandford", "The Investigator", 1);
+	Book b3 = new Book (33333, 2, "Danielle Steele", "High Stakes", 1);
+	
+	Library l1 = new Library();
+	
+	Librarian lb1 = new Librarian("Rebecca Gross", 2000851234, l1);
+	
+	Member m1 = new Member("Deja Pierce", 2000854321, l1);
+	Member m2 = new Member ("Elizabeth Jiang", 2000858524, l1);
+	
+	lb1.addBook(b1);
+	lb1.addBook(b2);
+	lb1.addBook(b3);
+	
+	m1.checkout(b2);
+	m2.checkout(b2);
+	b2.addtoWL(m2);
+	
+    System.out.println("The waitlist for " + b2.getTitle() + " by " + b2.getAuthor() + " contains the following members: " + b2.printwaitList());
 
   } 
 }

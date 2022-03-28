305c5875ade3302e34669702879b9d07b8c7bf14
ProvidSH <53474565+ProvidSH@users.noreply.github.com>
2022-03-28 17:02:18 -0400

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,34 +1,29 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Member extends Account{
 
     // instance variables
     private String name;
     private int memberID;
+    private Library library;
 
-    // list of books (listed by ID number) currently checked out by the member
-    private int[] checkedOut;
+    // list of books currently checked out by the member
+    private ArrayList<Book> checkedOut;
 
     // constructor
-    public Member(String nme){
+    public Member(String nme, int ID, Library library){
 
-        name = nme;
-        memberID = ID;
+        super(nme, ID);
+        this.library = library;
         // initialize list of checked out books to be empty
-        checkedOut = new int[0];
+        checkedOut = new ArrayList<>();
     }
     
     // setters and getters for the members
 
-    public String getName(){return name;}
-
-    public void setName(String n){name = name;}
-
-    public int getMemberID(){return memberID;}
-
-    public void setMemberID(int id){memberID = id;}
-
-    public int[] getCheckedOut(){return checkedOut;}
+    public ArrayList<Book> getCheckedOut() {
+    	return checkedOut;
+    }
 
     // methods
     
@@ -36,42 +31,35 @@ public class Member extends Account{
     public void checkout(Book b) {
 
         // check if the limit of checked out books is reached
-        if (getCheckedOut().length >= 5){
+        if (getCheckedOut().size() >= 5) {
             System.out.println("You have checked out the maximum number of books.\n Please return a book to check this one out.");
         }
 
-        else {
-
-            if (!b.getAvailable()){
+        else if (b.getAmount() == 0) { //if the book is unavailable
                 System.out.println("This book is currently not available. You will be added to the wait list for the book.");
                 addToWaitList(b);
             }
 
-            else {
-
-                checkedOut = Arrays.copyOf(checkedOut, checkedOut.length + 1);
-                checkedOut[checkedOut.length - 1] = b.getBookID();
-
-
-                b.setAvailable(false);
-            }
-        }
-    }
+        else { //check out the book
+                checkedOut.add(b); //add book to checkout list for member
+                library.getLOB().remove(b); //remove book from the list of books
+                b = new Book(b.getBookID(), b.getRackNumber(), b.getAuthor(), b.getTitle(), b.getAmount() - 1); 
+                // update number of books left
+         }
+     }
     
     // add the member to the wait list for an unavailable book
     public void addToWaitList(Book b){
-
-        b.getWaitList().add(getMemberID());
-
+        library.getWaitlist().add(b);
     }
 
     // get the position on the wait list for a given book
     public int waitListStatus(Book b){
         int position = 0;
 
-        for (int i = 0; i < b.getWaitList().size(); i++) {
+        for (int i = 0; i < library.getWaitlist().size(); i++) {
 
-            if (memberID == b.getWaitList().get(i)){
+            if (b == library.getWaitlist().get(i)){
                 position = i + 1;
             }
 

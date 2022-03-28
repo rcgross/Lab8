455bafcaa4cd0500d67594b7537e5319ed4d1ce7
rcgross <101359169+rcgross@users.noreply.github.com>
2022-03-27 20:14:56 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+
 public class Book {
     private int BookID; // Book ID number
     private int RackNumber; // Book Section/Rack number
     private boolean available = true; // this will indicate if a book is available, set to true if the book is available
                                         // for the library class if it is not available then put on waitlist in main class
                                         // if it is available then member can check out the book
-
+    private ArrayList<Integer> waitList;
+    
     // All the setter and getters for the instance variables
     public void setAvailable(boolean available)
     {
@@ -34,9 +37,11 @@ public class Book {
     }
 
     // constructor for a book, since each book has an ID number as well as a Rack Number
-    public Book (int bookID, int RackNumber) {
+    public Book (int bookID, int RackNumber, boolean available) {
         this.BookID = BookID;
         this.RackNumber = RackNumber;
+        this.available = available;
+        waitList = new ArrayList<>();
     }
 
 }
